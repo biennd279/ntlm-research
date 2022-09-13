3d5e103a724436cabd229d57a670e7d084c7ae17
biennd279 <bien.uet279@gmail.com>
2022-09-14 03:37:07 +0700

    Initial commit

---
+++ src/test/kotlin/InputDataBufferTest.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class InputDataBufferTest {
+
+    @Test
+    fun toByteArray() {
+    }
+
+    @Test
+    fun getIndex() {
+    }
+
+    @Test
+    fun peek() {
+    }
+
+    @Test
+    fun peekSafely() {
+    }
+
+    @Test
+    fun requireNextByte() {
+    }
+
+    @Test
+    fun nextByteOrNull() {
+    }
+
+    @Test
+    fun takeNext() {
+    }
+}
